d7e973e7c33707f86b0505a744ac260f10c8f805
Joao Martins <joao.martins@thecodeventure.com>
2023-10-18 00:26:57 +0100

    fix bug with tmux navigator

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,5 +1,6 @@
 return {
-  { "ggandor/leap.nvim", enabled = false },
-  { "ggandor/flit.nvim", enabled = false },
+  -- since lazy update this two were set as extras so no need to disable them
+  -- { "ggandor/leap.nvim", enabled = false },
+  -- { "ggandor/flit.nvim", enabled = false },
   { "folke/todo-comments.nvim", enabled = false },
 }
--- lua/plugins/tmux.lua
+++
@@ -1,6 +0,0 @@
-return {
-  -- vim tmux navigator
-  {
-    "christoomey/vim-tmux-navigator",
-  },
-}
